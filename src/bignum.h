vrancurel/dcss

commit 1327309a2a97aa6e328027e107fbcc34a17fe9c1

enable checks that advocate usage of modern language constructs.
The following warnings were fixed:
- modernize-loop-convert
- modernize-pass-by-value
- modernize-use-auto
    - when initializing with a cast (avoid duplicating the type name)
    - when initializing with new (avoid duplicating the type name)
    - when declaring iterators
- modernize-use-nullptr
- modernize-use-using

src/bignum.h
@@ -11,8 +11,8 @@
 
 #include <openssl/bn.h>
 
-typedef long long int64;
-typedef unsigned long long uint64;
+using int64 = long long;
+using uint64 = unsigned long long;
 
 /** Errors thrown by the bignum class. */
 class bignum_error : public std::runtime_error {
@@ -34,14 +34,14 @@ class CAutoBN_CTX {
     CAutoBN_CTX()
     {
         pctx = BN_CTX_new();
-        if (pctx == NULL) {
+        if (pctx == nullptr) {
             throw bignum_error("CAutoBN_CTX : BN_CTX_new() returned NULL");
         }
     }
 
     ~CAutoBN_CTX()
     {
-        if (pctx != NULL) {
+        if (pctx != nullptr) {
             BN_CTX_free(pctx);
         }
     }
@@ -63,7 +63,7 @@ class CAutoBN_CTX {
 
     bool operator!()
     {
-        return (pctx == NULL);
+        return (pctx == nullptr);
     }
 };
 
@@ -276,7 +276,7 @@ class CBigNum {
 
     std::vector<unsigned char> getvch() const
     {
-        unsigned int nSize = BN_bn2mpi(bn, NULL);
+        unsigned int nSize = BN_bn2mpi(bn, nullptr);
         if (nSize <= 4) {
             return std::vector<unsigned char>();
         }
@@ -611,7 +611,7 @@ inline const CBigNum operator/(const CBigNum& a, const CBigNum& b)
 {
     CAutoBN_CTX pctx;
     CBigNum r;
-    if (BN_div(r.bn, NULL, a.bn, b.bn, pctx) == 0) {
+    if (BN_div(r.bn, nullptr, a.bn, b.bn, pctx) == 0) {
         throw bignum_error("CBigNum::operator/ : BN_div failed");
     }
     return r;

src/kad_node.cpp
@@ -25,8 +25,6 @@ KadNode::KadNode(KadConf* conf, const CBigNum& id, const std::string& addr)
 
 KadNode::KadNode(KadConf* conf, const CBigNum& id) : KadNode(conf, id, "") {}
 
-KadNode::~KadNode() {}
-
 const std::string& KadNode::get_eth_account() const
 {
     return eth_account;
@@ -133,18 +131,17 @@ KadNode::find_nearest_nodes_local(const KadRoutable& routable, int amount)
     }
 
     if (!list.empty()) {
-        for (std::list<KadNode*>::iterator it = list.begin(); it != list.end();
-             ++it) {
+        for (auto& it : list) {
             if (count >= amount) {
                 break;
             }
 
             if (verbose) {
-                std::cout << (*it)->get_id().ToString(16) << " distance="
-                          << (*it)->distance_to(routable).ToString(16) << "\n";
+                std::cout << it->get_id().ToString(16) << " distance="
+                          << it->distance_to(routable).ToString(16) << "\n";
             }
 
-            closest.push_back(*it);
+            closest.push_back(it);
             count++;
         }
     }
@@ -160,18 +157,15 @@ KadNode::find_nearest_nodes_local(const KadRoutable& routable, int amount)
         for (int i = 1; i < (conf->n_bits + 1); i++) {
             if (bit_length != i) {
                 std::list<KadNode*>& list = buckets[i];
-                for (std::list<KadNode*>::iterator it = list.begin();
-                     it != list.end();
-                     ++it) {
+                for (auto& it : list) {
                     if (verbose) {
                         std::cout << "kbucket " << i << " "
-                                  << (*it)->get_id().ToString(16)
-                                  << " distance="
-                                  << (*it)->distance_to(routable).ToString(16)
+                                  << it->get_id().ToString(16) << " distance="
+                                  << it->distance_to(routable).ToString(16)
                                   << "\n";
                     }
 
-                    all.push_back(*it);
+                    all.push_back(it);
                 }
             }
         }
@@ -180,13 +174,12 @@ KadNode::find_nearest_nodes_local(const KadRoutable& routable, int amount)
         all.sort(routable);
         all.unique();
 
-        for (std::list<KadNode*>::iterator it = all.begin(); it != all.end();
-             ++it) {
+        for (auto& it : all) {
             if (count >= amount) {
                 break;
             }
 
-            closest.push_back(*it);
+            closest.push_back(it);
             count++;
         }
     }
@@ -239,19 +232,15 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
     }
 
     // Send find_nodes.
-    for (std::list<KadNode*>::iterator it = starting_nodes.begin();
-         it != starting_nodes.end();
-         ++it) {
-        std::list<KadNode*> answer =
-            (*it)->find_nearest_nodes(routable, conf->k);
+    for (auto& starting_node : starting_nodes) {
+        std::list<KadNode*> answers =
+            starting_node->find_nearest_nodes(routable, conf->k);
 
         // Add to answers.
-        for (std::list<KadNode*>::iterator it2 = answer.begin();
-             it2 != answer.end();
-             ++it2) {
+        for (auto& answer : answers) {
             // Remove oneself from the list.
-            if (get_id() != (*it2)->get_id()) {
-                answers.push_back(*it2);
+            if (get_id() != answer->get_id()) {
+                answers.push_back(answer);
             }
         }
     }
@@ -274,14 +263,12 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
         // Take only k answers.
         std::list<KadNode*> k_answers;
         u_int count = 0;
-        for (std::list<KadNode*>::iterator it = answers.begin();
-             it != answers.end();
-             ++it) {
+        for (auto& answer : answers) {
             if (count >= conf->k) {
                 break;
             }
 
-            k_answers.push_back(*it);
+            k_answers.push_back(answer);
 
             count++;
         }
@@ -292,7 +279,7 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
 
         std::list<KadNode*> round_answers;
 
-        std::list<KadNode*>::iterator it = k_answers.begin();
+        auto it = k_answers.begin();
         count = 0;
         while (true) {
             if (k_answers.end() == it) {
@@ -308,16 +295,14 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
                 break;
             }
 
-            std::list<KadNode*> answer =
+            std::list<KadNode*> answers =
                 (*it)->find_nearest_nodes(routable, conf->k);
 
             // Add to round_answers.
-            for (std::list<KadNode*>::iterator it2 = answer.begin();
-                 it2 != answer.end();
-                 ++it2) {
+            for (auto& answer : answers) {
                 // Remove oneself from the list.
-                if (get_id() != (*it2)->get_id()) {
-                    round_answers.push_back(*it2);
+                if (get_id() != answer->get_id()) {
+                    round_answers.push_back(answer);
                 }
             }
 
@@ -370,16 +355,14 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
                     continue;
                 }
 
-                std::list<KadNode*> answer =
+                std::list<KadNode*> answers =
                     (*it)->find_nearest_nodes(routable, conf->k);
 
                 // Add to answers.
-                for (std::list<KadNode*>::iterator it2 = answer.begin();
-                     it2 != answer.end();
-                     ++it2) {
+                for (auto& answer : answers) {
                     // Remove oneself from the list.
-                    if (get_id() != (*it2)->get_id()) {
-                        answers.push_back(*it2);
+                    if (get_id() != answer->get_id()) {
+                        answers.push_back(answer);
                     }
                 }
 
@@ -425,10 +408,8 @@ void KadNode::save(std::ostream& fout)
         if (!buckets[i].empty()) {
             fout << "bucket " << i << "\n";
             std::list<KadNode*>& list = buckets[i];
-            for (std::list<KadNode*>::iterator it = list.begin();
-                 it != list.end();
-                 ++it) {
-                fout << (*it)->get_id().ToString(16) << "\n";
+            for (auto& it : list) {
+                fout << it->get_id().ToString(16) << "\n";
             }
         }
     }
@@ -445,11 +426,9 @@ void KadNode::graphviz(std::ostream& fout)
     for (int i = 1; i < (conf->n_bits + 1); i++) {
         if (!buckets[i].empty()) {
             std::list<KadNode*>& list = buckets[i];
-            for (std::list<KadNode*>::iterator it = list.begin();
-                 it != list.end();
-                 ++it) {
+            for (auto& it : list) {
                 fout << "node_" << get_id().ToString(16) << " -> node_"
-                     << (*it)->get_id().ToString(16) << ";\n";
+                     << it->get_id().ToString(16) << ";\n";
             }
         }
     }

src/shell.cpp
@@ -11,7 +11,7 @@ const char* shell_err_strings[] = {
     "too many args",
     "argument is too long",
     "double quote error",
-    NULL,
+    nullptr,
 };
 
 const char* shell_error_str(enum shell_error err)
@@ -19,7 +19,7 @@ const char* shell_error_str(enum shell_error err)
     return (shell_err_strings[err]);
 }
 
-static struct cmd_def** g_cmd_defs = NULL;
+static struct cmd_def** g_cmd_defs = nullptr;
 
 /** For completion.
  *
@@ -49,24 +49,24 @@ static char* command_generator(const char* text, int state)
 
         if (strncmp(name, text, len) == 0) {
             char* nptr = strdup(name);
-            assert(NULL != nptr);
+            assert(nullptr != nptr);
             return nptr;
         }
     }
 
     /* If no names matched, then return NULL. */
-    return ((char*)NULL);
+    return ((char*)nullptr);
 }
 
 char** shell_completion(const char* text, int start, int end)
 {
     char** matches;
 
-    if (NULL == g_cmd_defs) {
-        return NULL;
+    if (nullptr == g_cmd_defs) {
+        return nullptr;
     }
 
-    matches = (char**)NULL;
+    matches = (char**)nullptr;
 
     if (start == 0) {
         matches = rl_completion_matches(text, command_generator);
@@ -84,10 +84,10 @@ Shell::Shell()
         Shell::class_initialized = true;
     }
 
-    defs = NULL;
-    handle = NULL;
-    handle2 = NULL;
-    prompt = NULL;
+    defs = nullptr;
+    handle = nullptr;
+    handle2 = nullptr;
+    prompt = nullptr;
 }
 
 int Shell::do_cmd(struct cmd_def** defs, int argc, char** argv)
@@ -119,7 +119,7 @@ int Shell::do_cmd(struct cmd_def** defs, int argc, char** argv)
             }
         } else {
         retry:
-            ret = wait(0);
+            ret = wait(nullptr);
             if (-1 == ret) {
                 if (EINTR == errno) {
                     goto retry;
@@ -131,13 +131,13 @@ int Shell::do_cmd(struct cmd_def** defs, int argc, char** argv)
         }
         return SHELL_CONT;
     }
-    if (NULL != (p = index(argv[0], '='))) {
+    if (nullptr != (p = index(argv[0], '='))) {
         *p++ = 0;
         return SHELL_CONT;
     }
     len = strlen(argv[0]);
 
-    def = NULL;
+    def = nullptr;
     found = 0;
     for (i = 0; defs[i] != nullptr; i++) {
         tmp = defs[i];
@@ -224,7 +224,7 @@ int Shell::parse(struct cmd_def** defs, char* str, enum shell_error* errp)
                 sargc++;
             }
 
-            sargv[sargc] = NULL;
+            sargv[sargc] = nullptr;
 
             if (dblquote) {
                 if (errp != nullptr) {
@@ -266,7 +266,7 @@ int Shell::parse(struct cmd_def** defs, char* str, enum shell_error* errp)
                 if ((sargc + 1) < SHELL_MAX_ARGV) {
                     sargv[sargc] = &sargmem[sargc][0];
                     sargc++;
-                    sargv[sargc] = NULL;
+                    sargv[sargc] = nullptr;
                     pos = 0;
                 } else {
                     if (errp != nullptr) {
@@ -330,7 +330,7 @@ void* Shell::get_handle2()
 void Shell::set_prompt(const char* prompt)
 {
     char* nprompt = strdup(prompt);
-    if (NULL == nprompt) {
+    if (nullptr == nprompt) {
         perror("strdup");
         exit(1);
     }
@@ -343,7 +343,7 @@ void Shell::loop()
     using_history();
 
     while (true) {
-        char* line = NULL;
+        char* line = nullptr;
 
         if ((line = readline(prompt)) != nullptr) {
             enum shell_error shell_err = SHELL_ERROR_NONE;

lint.rb
@@ -4,6 +4,7 @@
 COMPIL_OPTS = '-x c++ --std=c++11'.freeze
 CHECKS = %w[
   cert-*
+  modernize-*
   performance-*
   readability-*
 ].freeze

src/cmds.cpp
@@ -50,18 +50,18 @@ void cb_display_routable(const KadRoutable& routable, void* cb_arg)
 
 int cmd_rand_node(Shell* shell, int argc, char** argv)
 {
-    KadNetwork* network = (KadNetwork*)shell->get_handle();
+    auto* network = (KadNetwork*)shell->get_handle();
 
-    network->rand_node(cb_display_node, NULL);
+    network->rand_node(cb_display_node, nullptr);
 
     return SHELL_CONT;
 }
 
 int cmd_rand_key(Shell* shell, int argc, char** argv)
 {
-    KadNetwork* network = (KadNetwork*)shell->get_handle();
+    auto* network = (KadNetwork*)shell->get_handle();
 
-    network->rand_routable(cb_display_routable, NULL);
+    network->rand_routable(cb_display_routable, nullptr);
 
     return SHELL_CONT;
 }
@@ -73,10 +73,10 @@ int cmd_jump(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNetwork* network = (KadNetwork*)shell->get_handle();
+    auto* network = (KadNetwork*)shell->get_handle();
 
     KadNode* node = network->lookup_cheat(argv[1]);
-    if (NULL == node) {
+    if (nullptr == node) {
         fprintf(stderr, "not found\n");
         return SHELL_CONT;
     }
@@ -93,9 +93,9 @@ int cmd_lookup(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
+    auto* node = (KadNode*)shell->get_handle2();
 
-    if (NULL == node) {
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -122,9 +122,9 @@ int cmd_find_nearest(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
+    auto* node = (KadNode*)shell->get_handle2();
 
-    if (NULL == node) {
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -152,9 +152,9 @@ int cmd_show(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
+    auto* node = (KadNode*)shell->get_handle2();
 
-    if (NULL == node) {
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -171,9 +171,9 @@ int cmd_verbose(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
+    auto* node = (KadNode*)shell->get_handle2();
 
-    if (NULL == node) {
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -190,19 +190,19 @@ int cmd_cheat_lookup(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNetwork* network = (KadNetwork*)shell->get_handle();
+    auto* network = (KadNetwork*)shell->get_handle();
 
     CBigNum bn;
     bn.SetHex(argv[1]);
     KadRoutable routable(bn, KAD_ROUTABLE_FILE);
 
     KadNode* node = network->find_nearest_cheat(routable);
-    if (NULL == node) {
+    if (nullptr == node) {
         fprintf(stderr, "not found\n");
         return SHELL_CONT;
     }
 
-    cb_display_node(node, NULL);
+    cb_display_node(node, nullptr);
 
     return SHELL_CONT;
 }
@@ -214,7 +214,7 @@ int cmd_save(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNetwork* network = (KadNetwork*)shell->get_handle();
+    auto* network = (KadNetwork*)shell->get_handle();
 
     std::ofstream fout(argv[1]);
     network->save(fout);
@@ -264,7 +264,7 @@ int cmd_graphviz(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNetwork* network = (KadNetwork*)shell->get_handle();
+    auto* network = (KadNetwork*)shell->get_handle();
 
     std::ofstream fout(argv[1]);
     network->graphviz(fout);
@@ -279,8 +279,8 @@ int cmd_buy_storage(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
-    if (NULL == node) {
+    auto* node = (KadNode*)shell->get_handle2();
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -297,8 +297,8 @@ int cmd_put_bytes(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
-    if (NULL == node) {
+    auto* node = (KadNode*)shell->get_handle2();
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -315,8 +315,8 @@ int cmd_get_bytes(Shell* shell, int argc, char** argv)
         return SHELL_CONT;
     }
 
-    KadNode* node = (KadNode*)shell->get_handle2();
-    if (NULL == node) {
+    auto* node = (KadNode*)shell->get_handle2();
+    if (nullptr == node) {
         fprintf(stderr, "shall jump to a node first\n");
         return SHELL_CONT;
     }
@@ -379,5 +379,5 @@ struct cmd_def* cmd_defs[] = {
     &show_cmd,
     &verbose_cmd,
     &xor_cmd,
-    NULL,
+    nullptr,
 };

src/kad_file.cpp
@@ -6,8 +6,6 @@ KadFile::KadFile(const CBigNum& id, KadNode* referencer)
     this->referencer = referencer;
 }
 
-KadFile::~KadFile() {}
-
 KadNode* KadFile::get_referencer()
 {
     return referencer;

src/kad_network.cpp
@@ -9,8 +9,6 @@ KadNetwork::KadNetwork(KadConf* conf)
     this->conf = conf;
 }
 
-KadNetwork::~KadNetwork() {}
-
 /**
  * initialize nodes
  *
@@ -104,15 +102,13 @@ void KadNetwork::initialize_files(int n_files)
         // Gen a random identifier for the file.
         CBigNum bn;
         bn.Rand(conf->n_bits);
-        KadFile* file = new KadFile(bn, node);
+        auto* file = new KadFile(bn, node);
         files.push_back(file);
 
         // Store file at multiple location.
         std::list<KadNode*> result = node->lookup(*file);
-        for (std::list<KadNode*>::iterator it = result.begin();
-             it != result.end();
-             ++it) {
-            (*it)->store(file);
+        for (auto& it : result) {
+            it->store(file);
         }
     }
 }
@@ -141,12 +137,9 @@ void KadNetwork::check_files()
 
         // Check that at least one result has the file.
         bool found = false;
-        for (std::list<KadNode*>::iterator it = result.begin();
-             it != result.end();
-             ++it) {
-            std::vector<KadFile*> node_files = (*it)->get_files();
-            for (u_int j = 0; j < node_files.size(); j++) {
-                KadFile* node_file = node_files[j];
+        for (auto& it : result) {
+            std::vector<KadFile*> node_files = it->get_files();
+            for (auto node_file : node_files) {
                 if (node_file == file) {
                     found = true;
                     break;
@@ -169,7 +162,7 @@ void KadNetwork::rand_node(tnode_callback_func cb_func, void* cb_arg)
 {
     std::uniform_int_distribution<> dis(0, nodes.size() - 1);
     int x = dis(prng());
-    if (NULL != cb_func) {
+    if (nullptr != cb_func) {
         cb_func(nodes[x], cb_arg);
     }
 }
@@ -179,7 +172,7 @@ void KadNetwork::rand_routable(troutable_callback_func cb_func, void* cb_arg)
     CBigNum bn;
     bn.Rand(conf->n_bits);
     KadRoutable routable(bn, KAD_ROUTABLE_FILE);
-    if (NULL != cb_func) {
+    if (nullptr != cb_func) {
         cb_func(routable, cb_arg);
     }
 }
@@ -203,17 +196,17 @@ KadNode* KadNetwork::lookup_cheat(const std::string& id)
  */
 KadNode* KadNetwork::find_nearest_cheat(const KadRoutable& routable)
 {
-    KadNode* nearest = NULL;
+    KadNode* nearest = nullptr;
 
-    for (u_int i = 0; i < nodes.size(); i++) {
-        if (NULL == nearest) {
-            nearest = nodes[i];
+    for (auto& node : nodes) {
+        if (nullptr == nearest) {
+            nearest = node;
         } else {
-            CBigNum d1 = nodes[i]->distance_to(routable);
+            CBigNum d1 = node->distance_to(routable);
             CBigNum d2 = nearest->distance_to(routable);
 
             if (d1 < d2) {
-                nearest = nodes[i];
+                nearest = node;
             }
         }
     }

src/kad_routable.cpp
@@ -6,8 +6,6 @@ KadRoutable::KadRoutable(const CBigNum& id, enum KadRoutableType type)
     this->type = type;
 }
 
-KadRoutable::~KadRoutable() {}
-
 CBigNum KadRoutable::get_id() const
 {
     return id;

commit 8136ccca48f51c1a5ad7006394488a6674da9077

add `-Wshadow`
This actually spotted some real bugs introduced in
1327309a2a97aa6e328027e107fbcc34a17fe9c1 where I introduced some
`answers` variables that were hiding other ones with a wider scope.

src/Makefile
@@ -19,6 +19,7 @@ CFLAGS = \
 -Wnon-virtual-dtor \
 -Wold-style-cast \
 -Woverlength-strings \
+-Wshadow \
 -Wshift-sign-overflow \
 -Wstring-compare -Wstring-conversion -Wstring-plus-char \
 -Wsometimes-uninitialized \

src/bignum.h
@@ -415,18 +415,18 @@ class CBigNum {
         CBigNum bnBase(nBase);
         CBigNum bn0(0);
         std::string str;
-        CBigNum bn = *this;
-        BN_set_negative(bn.bn, 0);
+        CBigNum tmp = *this;
+        BN_set_negative(tmp.bn, 0);
         CBigNum dv;
         CBigNum rem;
-        if (BN_cmp(bn.bn, bn0.bn) == 0) {
+        if (BN_cmp(tmp.bn, bn0.bn) == 0) {
             return "0";
         }
-        while (BN_cmp(bn.bn, bn0.bn) > 0) {
-            if (BN_div(dv.bn, rem.bn, bn.bn, bnBase.bn, pctx) == 0) {
+        while (BN_cmp(tmp.bn, bn0.bn) > 0) {
+            if (BN_div(dv.bn, rem.bn, tmp.bn, bnBase.bn, pctx) == 0) {
                 throw bignum_error("CBigNum::ToString() : BN_div failed");
             }
-            bn = dv;
+            tmp = dv;
             uint64 c = rem.getulong();
             str += "0123456789abcdef"[c];
         }

src/kad_node.cpp
@@ -150,8 +150,8 @@ KadNode::find_nearest_nodes_local(const KadRoutable& routable, int amount)
         // Find remaining nearest nodes.
         for (int i = 1; i < (conf->n_bits + 1); i++) {
             if (bit_length != i) {
-                std::list<KadNode*>& list = buckets[i];
-                for (auto& it : list) {
+                std::list<KadNode*>& nodes = buckets[i];
+                for (auto& it : nodes) {
                     if (verbose) {
                         std::cout << "kbucket " << i << " "
                                   << it->get_id().ToString(16) << " distance="
@@ -218,11 +218,11 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
 
     // Send find_nodes.
     for (auto& starting_node : starting_nodes) {
-        std::list<KadNode*> answers =
+        std::list<KadNode*> nodes =
             starting_node->find_nearest_nodes(routable, conf->k);
 
         // Add to answers.
-        for (auto& answer : answers) {
+        for (auto& answer : nodes) {
             // Remove oneself from the list.
             if (get_id() != answer->get_id()) {
                 answers.push_back(answer);
@@ -280,11 +280,11 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
                 break;
             }
 
-            std::list<KadNode*> answers =
+            std::list<KadNode*> nodes =
                 (*it)->find_nearest_nodes(routable, conf->k);
 
             // Add to round_answers.
-            for (auto& answer : answers) {
+            for (auto& answer : nodes) {
                 // Remove oneself from the list.
                 if (get_id() != answer->get_id()) {
                     round_answers.push_back(answer);
@@ -340,11 +340,11 @@ std::list<KadNode*> KadNode::lookup(const KadRoutable& routable)
                     continue;
                 }
 
-                std::list<KadNode*> answers =
+                std::list<KadNode*> nodes =
                     (*it)->find_nearest_nodes(routable, conf->k);
 
                 // Add to answers.
-                for (auto& answer : answers) {
+                for (auto& answer : nodes) {
                     // Remove oneself from the list.
                     if (get_id() != answer->get_id()) {
                         answers.push_back(answer);

src/shell.cpp
@@ -81,7 +81,7 @@ Shell::Shell()
     handle2 = nullptr;
 }
 
-int Shell::do_cmd(struct cmd_def** defs, int argc, char** argv)
+int Shell::do_cmd(struct cmd_def** definitions, int argc, char** argv)
 {
     struct cmd_def *def, *tmp;
     size_t len;
@@ -131,8 +131,8 @@ int Shell::do_cmd(struct cmd_def** defs, int argc, char** argv)
 
     def = nullptr;
     found = 0;
-    for (i = 0; defs[i] != nullptr; i++) {
-        tmp = defs[i];
+    for (i = 0; definitions[i] != nullptr; i++) {
+        tmp = definitions[i];
         if (strcmp(argv[0], tmp->name) == 0) {
             found = 1;
             def = tmp;
@@ -164,13 +164,16 @@ int Shell::do_cmd(struct cmd_def** defs, int argc, char** argv)
  * Understands comments (sharp sign), double quotes, semicolon. ignore
  * whitspaces
  *
- * @param defs the callback
+ * @param definitions the callback
  * @param str the input string
  * @param errp the error code
  *
  * @return 0 if OK, -1 on failure
  */
-int Shell::parse(struct cmd_def** defs, char* str, enum shell_error* errp)
+int Shell::parse(
+    struct cmd_def** definitions,
+    char* str,
+    enum shell_error* errp)
 {
     int pos, ret;
     bool comment = false;
@@ -225,7 +228,7 @@ int Shell::parse(struct cmd_def** defs, char* str, enum shell_error* errp)
                 return -1;
             }
 
-            if ((ret = do_cmd(defs, sargc, sargv)) != SHELL_CONT) {
+            if ((ret = do_cmd(definitions, sargc, sargv)) != SHELL_CONT) {
                 return ret;
             }
 
@@ -292,19 +295,19 @@ int Shell::parse(struct cmd_def** defs, char* str, enum shell_error* errp)
     }
 }
 
-void Shell::set_cmds(struct cmd_def** defs)
+void Shell::set_cmds(struct cmd_def** definitions)
 {
-    g_cmd_defs = this->defs = defs;
+    g_cmd_defs = this->defs = definitions;
 }
 
-void Shell::set_handle(void* handle)
+void Shell::set_handle(void* hdl)
 {
-    this->handle = handle;
+    this->handle = hdl;
 }
 
-void Shell::set_handle2(void* handle)
+void Shell::set_handle2(void* hdl)
 {
-    this->handle2 = handle;
+    this->handle2 = hdl;
 }
 
 void* Shell::get_handle()
@@ -317,9 +320,9 @@ void* Shell::get_handle2()
     return handle2;
 }
 
-void Shell::set_prompt(const std::string& prompt)
+void Shell::set_prompt(const std::string& ps1)
 {
-    this->prompt = prompt;
+    this->prompt = ps1;
 }
 
 void Shell::loop()

src/shell.h
@@ -42,10 +42,10 @@ class Shell {
   public:
     Shell();
 
-    void set_cmds(struct cmd_def** defs);
-    void set_handle(void* handle);
-    void set_handle2(void* handle);
-    void set_prompt(const std::string& prompt);
+    void set_cmds(struct cmd_def** definitions);
+    void set_handle(void* hdl);
+    void set_handle2(void* hdl);
+    void set_prompt(const std::string& ps1);
     void* get_handle();
     void* get_handle2();
     void loop();
@@ -57,8 +57,8 @@ class Shell {
     std::string prompt;
     static bool class_initialized;
 
-    int do_cmd(struct cmd_def** defs, int argc, char** argv);
-    int parse(struct cmd_def** defs, char* str, enum shell_error* errp);
+    int do_cmd(struct cmd_def** definitions, int argc, char** argv);
+    int parse(struct cmd_def** definitions, char* str, enum shell_error* errp);
 };
 
 #endif