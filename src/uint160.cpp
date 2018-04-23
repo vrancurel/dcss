eddfc6c87b6b41b82236630224d76e8c1cc21b3b
Sylvain Laperche <sylvain.laperche@scality.com>
2018-04-24 01:00:14 +0200

    uint160: implement a 160-bit unsigned integer
    Constructors from unsigned integer are implicit because it makes code
    easier to read/write (and also more natural).
    
    Conversion from string and to boolean still require an explicit call
    though.

---
+++ src/uint160.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2017-2018 the QuadIron authors
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "exceptions.h"
+#include "uint160.h"
+
+namespace kad {
+
+// Return the integral value of an hex digit.
+static inline uint32_t decode_hex_char(char hex)
+{
+    if (hex >= '0' && hex <= '9') {
+        return hex - '0';
+    }
+    if (hex >= 'a' && hex <= 'z') {
+        return hex - 'a' + 0xA;
+    }
+    if (hex >= 'A' && hex <= 'Z') {
+        return hex - 'A' + 0xA;
+    }
+    throw Exception("invalid hex string: bad character");
+}
+
+UInt160::UInt160(uint64_t n) : UInt160()
+{
+    m_limbs[m_limbs.size() - 1] = static_cast<uint32_t>(n >> 0u);
+    m_limbs[m_limbs.size() - 2] = static_cast<uint32_t>(n >> 32u);
+}
+
+UInt160::UInt160(const std::string& hex) : UInt160()
+{
+    // TODO: be less strict?
+    if (hex.size() != HEX_SIZE) {
+        throw LogicError("invalid hex string: bad length");
+    }
+    // Iterate by chunk of 8 char: 32-bit is represented by 8 hex char.
+    for (std::string::size_type i = 0; i != hex.size(); i += 8) {
+        m_limbs[i / 8] = (decode_hex_char(hex[i + 0]) << 28u)
+                         | (decode_hex_char(hex[i + 1]) << 24u)
+                         | (decode_hex_char(hex[i + 2]) << 20u)
+                         | (decode_hex_char(hex[i + 3]) << 16u)
+                         | (decode_hex_char(hex[i + 4]) << 12u)
+                         | (decode_hex_char(hex[i + 5]) << 8u)
+                         | (decode_hex_char(hex[i + 6]) << 4u)
+                         | (decode_hex_char(hex[i + 7]) << 0u);
+    }
+}
+
+UInt160 UInt160::rand(std::mt19937& prng)
+{
+    UInt160 n{};
+
+    for (auto& limb : n.m_limbs) {
+        limb = static_cast<uint32_t>(prng());
+    }
+
+    return n;
+}
+
+std::string UInt160::to_string() const
+{
+    static const char charset[] = "0123456789abcdef";
+    std::string hex;
+
+    hex.reserve(HEX_SIZE);
+    for (const auto& limb : m_limbs) {
+        hex.push_back(charset[(limb >> 28u) & 0xFu]);
+        hex.push_back(charset[(limb >> 24u) & 0xFu]);
+        hex.push_back(charset[(limb >> 20u) & 0xFu]);
+        hex.push_back(charset[(limb >> 16u) & 0xFu]);
+        hex.push_back(charset[(limb >> 12u) & 0xFu]);
+        hex.push_back(charset[(limb >> 8u) & 0xFu]);
+        hex.push_back(charset[(limb >> 4u) & 0xFu]);
+        hex.push_back(charset[(limb >> 0u) & 0xFu]);
+    }
+    return hex;
+}
+
+bool operator==(const UInt160& lhs, const UInt160& rhs)
+{
+    return lhs.m_limbs == rhs.m_limbs;
+}
+
+std::ostream& operator<<(std::ostream& os, const UInt160& n)
+{
+    // TODO: handle formatter such as std::dec, std::hex and std::oct?
+    return os << n.to_string();
+}
+
+} // namespace kad
---
+++ test/uint160.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017-2018 the QuadIron authors
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <cstdint>
+#include <sstream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "exceptions.h"
+#include "uint160.h"
+
+template <typename T>
+std::string int_to_hex(T n)
+{
+    std::stringstream stream;
+
+    stream << std::setfill('0') << std::setw(40) << std::hex << n;
+    return stream.str();
+}
+
+TEST(UInt160Test, TestInitDefault) // NOLINT
+{
+    const kad::UInt160 n{};
+    const kad::UInt160 zero(0u);
+
+    // NOLINTNEXTLINE(hicpp-vararg)
+    EXPECT_EQ(n, zero) << "default constructor return the value 0";
+}
+
+TEST(UInt160Test, TestInitFromInt) // NOLINT
+{
+    const uint64_t values[] = {0, 1, 255, 65000, 4000000, 10000000000};
+
+    for (auto val : values) {
+        const std::string result(kad::UInt160(val).to_string());
+        const std::string expected(int_to_hex(val));
+
+        // NOLINTNEXTLINE(hicpp-vararg)
+        EXPECT_EQ(result, expected) << "testing init from " << val;
+    }
+}
+
+TEST(UInt160Test, TestInitFromHex) // NOLINT
+{
+    const std::string hex("c544b5e4a1afcbb5d2de772d7a8df76f32557147");
+    kad::UInt160 n(hex);
+
+    // NOLINTNEXTLINE(hicpp-vararg)
+    EXPECT_EQ(n.to_string(), hex) << "testing init from " << hex;
+
+    ASSERT_THROW(kad::UInt160("deadbeef"), kad::LogicError)
+        << "bad hex string size";
+
+    ASSERT_THROW(
+        kad::UInt160("One cannot step twice in the same river."),
+        kad::Exception)
+        << "bad string (not an hex string)";
+}
