f2ff3fc5d47c968adc4c0b587ac4cdfb52031f4e
Sylvain Laperche <sylvain.laperche@scality.com>
2018-05-18 08:06:35 +0200

    define the output operator for NodeAddress
    This will make it easier to use with logging.

--- src/dht/address.cpp
+++ src/dht/address.cpp
@@ -27,77 +27,16 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef __KAD_DHT_ADDRESS_H__
-#define __KAD_DHT_ADDRESS_H__
-
-#include <cstdint>
-
-#include "uint160.h"
+#include "address.h"
 
 namespace kad {
 namespace dht {
 
-// TODO: just a placeholder for now, should be implemented later.
-class IpAddress {
-  public:
-    explicit IpAddress(const std::string& ip)
-    {
-        // TODO: parse the IP using `inet_pton`
-        (void)ip;
-    }
-};
-
-class NodeAddress {
-  public:
-    NodeAddress(UInt160 id, const std::string& ip, uint16_t port)
-        : m_id(id), m_ip(ip), m_port(port)
-    {
-    }
-
-    /** Return the node's ID. */
-    inline const UInt160& id() const
-    {
-        return m_id;
-    };
-
-    /** Return the node's IP. */
-    inline const IpAddress& ip() const
-    {
-        return m_ip;
-    };
-
-    /** Return the node's port. */
-    inline uint16_t port() const
-    {
-        return m_port;
-    };
-
-    inline bool operator==(const NodeAddress& other) const
-    {
-        return m_id == other.m_id;
-    }
-
-    // Output operator.
-    friend std::ostream& operator<<(std::ostream& os, const NodeAddress& addr);
-
-  private:
-    UInt160 m_id;
-    IpAddress m_ip;
-    uint16_t m_port;
-};
+std::ostream& operator<<(std::ostream& os, const NodeAddress& addr)
+{
+    // TODO: add the other fields (such as IP:port)?
+    return os << addr.m_id.to_string();
+}
 
 } // namespace dht
 } // namespace kad
-
-// Implementing std::hash for dht::NodeAddress.
-namespace std {
-template <>
-struct hash<kad::dht::NodeAddress> {
-    size_t operator()(const kad::dht::NodeAddress& addr) const
-    {
-        return hash<kad::UInt160>()(addr.id());
-    }
-};
-} // namespace std
-
-#endif
